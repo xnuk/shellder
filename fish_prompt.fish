cc8215d5c578b075fedc447d2ae65fbc6fc2077b
Xnuk Shuman <admin@xnu.kr>
2021-07-08 22:08:03 +0900

    이 참에 안 쓰는 거 치우기

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -7,23 +7,6 @@
 
 
 
-# Backward compatibility
-#
-# Note: Do not depend on these behavior. These can be removed in anytime by the
-# author in the name of code readability.
-if set -q theme_hide_hostname
-  # Existing $theme_hostname will always override $theme_hide_hostname
-  if not set -q theme_hostname
-    if [ "theme_hide_hostname" = "yes" ]
-      set -g theme_hostname never
-    end
-    if [ "theme_hide_hostname" = "no" ]
-      set -g theme_hostname always
-    end
-  end
-end
-
-
 #
 # Segments functions
 #
@@ -117,26 +100,6 @@ function prompt_dir -d "Display the current directory"
 end
 
 
-function prompt_hg -d "Display mercurial state"
-  set -l branch
-  set -l state
-  if command hg id >/dev/null 2>&1
-    if command hg prompt >/dev/null 2>&1
-      set branch (command hg prompt "{branch}")
-      set state (command hg prompt "{status}")
-      set branch_symbol \uE0A0
-      if [ "$state" = "!" ]
-        prompt_segment red white "$branch_symbol $branch ±"
-      else if [ "$state" = "?" ]
-          prompt_segment yellow black "$branch_symbol $branch ±"
-        else
-          prompt_segment green black "$branch_symbol $branch"
-      end
-    end
-  end
-end
-
-
 function prompt_git -d "Display the current git state"
   set -l ref
   if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
@@ -192,34 +155,6 @@ function prompt_git -d "Display the current git state"
 end
 
 
-function prompt_svn -d "Display the current svn state"
-  set -l ref
-  if command svn ls . >/dev/null 2>&1
-    set branch (svn_get_branch)
-    set branch_symbol \uE0A0
-    set revision (svn_get_revision)
-    prompt_segment green black "$branch_symbol $branch:$revision"
-  end
-end
-
-function svn_get_branch -d "get the current branch name"
-  svn info 2> /dev/null | awk -F/ \
-      '/^URL:/ { \
-        for (i=0; i<=NF; i++) { \
-          if ($i == "branches" || $i == "tags" ) { \
-            print $(i+1); \
-            break;\
-          }; \
-          if ($i == "trunk") { print $i; break; } \
-        } \
-      }'
-end
-
-function svn_get_revision -d "get the current revision number"
-  svn info 2> /dev/null | sed -n 's/Revision:\ //p'
-end
-
-
 function prompt_status -d "the symbols for a non zero exit status, root and background jobs"
     if [ $RETVAL -ne 0 ]
       prompt_segment black red "✘"
@@ -237,30 +172,12 @@ function prompt_status -d "the symbols for a non zero exit status, root and back
     end
 end
 
-if printf '%s\n' '2.2.0' $FISH_VERSION | sort --check=silent --version-sort
-  # Current version ≥ 2.2.0
-  function __exists -a name -d "Check if a function or program does exist."
-    command -v "$name" ^/dev/null >&2
-  end
-else
-  # Current version < 2.2.0
-  function __exists -a name -d "Check if a function or program does exist."
-    type "$name" ^/dev/null >&2
-  end
-end
-
-
-#
-# Prompt
-#
 function fish_prompt
   set -g RETVAL $status
   prompt_status
   prompt_virtual_env
   prompt_user
   prompt_dir
-  __exists hg;  and prompt_hg
-  __exists git; and prompt_git
-  __exists svn; and prompt_svn
+  prompt_git
   prompt_finish
 end
